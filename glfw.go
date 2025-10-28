24f63cc77a069567ba1ebb735d85c214d2a4270f
Lukasz Kwiatkowski <kontakt.cold0@gmail.com>
2025-10-28 10:11:15 +0000

    Add GLFW constants and function signatures

---
+++ glfw.go
@@ -0,0 +1,13 @@
+var GLFW_SAMPLES int = 0x0002100D
+var GLFW_CONTEXT_VERSION_MAJOR int = 0x00022002
+var GLFW_CONTEXT_VERSION_MINOR int = 0x00022003
+var GLFW_OPENGL_FORWARD_COMPAT int = 0x00022006
+var GLFW_OPENGL_PROFILE int = 0x00022008
+var GLFW_OPENGL_CORE_PROFILE int = 0x00032001
+
+var GlfwInit func() int
+var GlfwWindowHint func(hint int, value int)
+var GlfwCreateWindow func(width int, height int, title string, monitor uintptr, share uintptr) uintptr
+var GlfwTerminate func()
+var GlfwMakeContextCurrent func(window uintptr)
+var GlfwSetInputMode func(
