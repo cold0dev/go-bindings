33d258510de794c67ab7e976c183a858ac672ff5
Lukasz Kwiatkowski <kontakt.cold0@gmail.com>
2025-10-30 13:28:00 +0000

    Add files via upload

--- glfw.go
+++ glfw.go
@@ -1,16 +1,45 @@
 package main
 
+import "github.com/ebitengine/purego"
+
 var GLFW_SAMPLES int = 0x0002100D
 var GLFW_CONTEXT_VERSION_MAJOR int = 0x00022002
 var GLFW_CONTEXT_VERSION_MINOR int = 0x00022003
 var GLFW_OPENGL_FORWARD_COMPAT int = 0x00022006
 var GLFW_OPENGL_PROFILE int = 0x00022008
 var GLFW_OPENGL_CORE_PROFILE int = 0x00032001
+var GLFW_STICKY_KEYS int = 0x00033002
+var GLFW_KEY_ESCAPE int = 256
+var GLFW_PRESS int = 1
+
 
 var GlfwInit func() int
 var GlfwWindowHint func(hint int, value int)
 var GlfwCreateWindow func(width int, height int, title string, monitor uintptr, share uintptr) uintptr
 var GlfwTerminate func()
 var GlfwMakeContextCurrent func(window uintptr)
-var GlfwSetInputMode func(
+var GlfwSetInputMode func(window uintptr, mode int, value int)
+var GlfwSwapBuffers func (window uintptr)
+var GlfwPollEvents func()
+var GlfwGetKey func(window uintptr, key int) int
+var GlfwWindowShouldClose func(window uintptr) int
 
+var GLFWPath string = "/usr/lib/libglfw.so"
+
+func LoadGLFW() {
+	lib, err := purego.Dlopen(GLFWPath, purego.RTLD_NOW|purego.RTLD_GLOBAL)
+	if err != nil {
+		panic(err)
+	}
+
+	purego.RegisterLibFunc(&GlfwInit, lib, "glfwInit")
+	purego.RegisterLibFunc(&GlfwWindowHint, lib, "glfwWindowHint")
+	purego.RegisterLibFunc(&GlfwCreateWindow, lib, "glfwCreateWindow")
+	purego.RegisterLibFunc(&GlfwTerminate, lib, "glfwTerminate")
+	purego.RegisterLibFunc(&GlfwMakeContextCurrent, lib, "glfwMakeContextCurrent")
+	purego.RegisterLibFunc(&GlfwSetInputMode, lib, "glfwSetInputMode")
+	purego.RegisterLibFunc(&GlfwSwapBuffers, lib, "glfwSwapBuffers")
+	purego.RegisterLibFunc(&GlfwPollEvents, lib, "glfwPollEvents")
+	purego.RegisterLibFunc(&GlfwGetKey, lib, "glfwGetKey")
+	purego.RegisterLibFunc(&GlfwWindowShouldClose, lib, "glfwWindowShouldClose")
+}
--- opengl.go
+++ opengl.go
@@ -2,12 +2,22 @@ package main
 
 import "github.com/ebitengine/purego"
 
-var OpenGLPath string = "/System/Library/Frameworks/OpenGL.framework/OpenGL"
+var OpenGLPathMac string = "/System/Library/Frameworks/OpenGL.framework/OpenGL"
+var OpenGLPathLinux string = "/usr/lib/libOpenGL.so"
+
+type GLbitfield uint
+
+var GL_COLOR_BUFFER_BIT GLbitfield = 0x00004000
+var GL_DEPTH_BUFFER_BIT GLbitfield = 0x00000100
+var GL_TRUE int = 1
+
+var GlClear func(mask GLbitfield)
 
 func LoadOpenGL() {
-	_, err := purego.Dlopen(OpenGLPath, purego.RTLD_NOW|purego.RTLD_GLOBAL)
+	lib, err := purego.Dlopen(OpenGLPathLinux, purego.RTLD_NOW|purego.RTLD_GLOBAL)
 	if err != nil {
 		panic(err)
 	}
-	println("Done")
+
+	purego.RegisterLibFunc(&GlClear, lib, "glClear")
 }
